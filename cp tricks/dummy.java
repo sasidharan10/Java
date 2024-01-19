9ea0d900594789b5746513488f3ae987c528cc37
sasidharan10 <ksasidharan98@gmail.com>
2024-01-19 12:43:27 +0530

    java

--- cp tricks/dummy.java
+++ cp tricks/dummy.java
@@ -1,31 +1,30 @@
 import java.util.*;
 
 public class dummy {
-    public static List<List<String>> groupAnagrams(String[] strs) {
-        int n = strs.length;
-        HashMap<String, ArrayList<String>> mp = new HashMap<>();
+    public static boolean uniqueOccurrences(int[] arr) {
+        int n = arr.length;
+        HashMap<Integer, Integer> mp = new HashMap();
+        HashSet<Integer> set = new HashSet();
         for (int i = 0; i < n; i++) {
-            String temp = strs[i];
-            char[] charArray = temp.toCharArray();
-            Arrays.sort(charArray);
-            temp = new String(charArray);
-            if (!mp.containsKey(temp)) {
-                mp.put(temp, new ArrayList<>());
+            if (!mp.containsKey(arr[i]))
+                mp.put(arr[i], 1);
+            else {
+                mp.replace(arr[i], mp.get(arr[i]) + 1);
             }
-            mp.get(temp).add(strs[i]);
         }
-        List<List<String>> ans = new ArrayList<>();
-
-        for (Map.Entry<String, ArrayList<String>> it : mp.entrySet()) {
-            ans.add(it.getValue());
+        for (Map.Entry<Integer, Integer> it : mp.entrySet()) {
+            if (set.contains(it.getValue()))
+                return false;
+            else
+                set.add(it.getValue());
         }
-        return ans;
+        return true;
     }
 
     public static void main(String[] args) {
-        String[] nums = { "eat", "tea", "tan", "ate", "nat", "bat" };
+        int[] nums = { -3, 0, 1, -3, 1, 1, 1, -3, 10, 0 };
         System.out.print("Result: ");
-        System.out.print(groupAnagrams(nums));
+        System.out.print(uniqueOccurrences(nums));
     }
 
 }
