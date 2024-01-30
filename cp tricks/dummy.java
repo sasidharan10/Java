f1b003ee183c0e83e46caa1ba0edab2fba64561b
sasidharan10 <ksasidharan98@gmail.com>
2024-01-30 17:55:28 +0530

    more on collections

--- cp tricks/dummy.java
+++ cp tricks/dummy.java
@@ -1,45 +1,31 @@
 import java.util.*;
 
 public class dummy {
-    public static int solve(int ind, int val, int n, List<Integer> nums) {
-        if (ind == n) {
-            return Integer.bitCount(val);
-        }
-        int notTake = Integer.MIN_VALUE, take = Integer.MIN_VALUE;
-        if ((val & nums.get(ind)) != 0) {
-            notTake = solve(ind + 1, val, n, nums);
-        } else {
-            notTake = solve(ind + 1, val, n, nums);
-            take = solve(ind + 1, val | nums.get(ind), n, nums);
-        }
-        return Math.max(take, notTake);
-    }
 
-    public static int maxLength(List<String> arr) {
-        int n = arr.size();
-        List<Integer> nums = new ArrayList<>();
+    public static int subarraySum(int[] nums, int k) {
+        int n = nums.length;
+        HashMap<Integer, Integer> mp = new HashMap<>();
+        mp.put(0, 1);
+        int cnt = 0;
+        int cumSum = 0;
         for (int i = 0; i < n; i++) {
-            Set<Character> st = new HashSet<>();
-            for (char ch : arr.get(i).toCharArray()) {
-                st.add(ch);
+            cumSum += nums[i];
+            if (mp.containsKey(cumSum - k)) {
+                cnt += mp.get(cumSum - k);
             }
-            if (st.size() != arr.get(i).length())
-                continue;
-            int val = 0;
-            for (char ch : arr.get(i).toCharArray()) {
-                val = val | (1 << (ch - 'a'));
-            }
-            nums.add(val);
+            if (mp.containsKey(cumSum))
+                mp.replace(cumSum, mp.get(cumSum) + 1);
+            else
+                mp.put(cumSum, 1);
         }
-        int m = nums.size();
-        return solve(0, 0, m, nums);
+        return cnt;
     }
 
     public static void main(String[] args) {
-        String[] nums = { "cha", "r", "act", "ers" };
-        List<String> arr = new ArrayList<>(Arrays.asList(nums));
+        int[] nums = { 1, 2, 3 };
+        int k = 3;
         System.out.print("Result: ");
-        System.out.print(maxLength(arr));
+        System.out.print(subarraySum(nums, k));
     }
 
 }
--- cp tricks/moreOnCollections.java
+++ cp tricks/moreOnCollections.java
@@ -3,6 +3,15 @@ import java.util.*;
 public class moreOnCollections {
 
     public static void main(String[] args) {
-        
+        HashMap<Integer, Integer> mp = new HashMap<>();
+        mp.put(1, 0);
+        mp.put(2, 0);
+        mp.put(3, 0);
+
+        // to increment value, if exists
+        mp.put(1, mp.getOrDefault(1, 0) + 1);
+
+        System.out.println(mp);
+
     }
 }
