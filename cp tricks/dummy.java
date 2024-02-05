bfb0793ef291de664875a7a11e87d0cae9db2f8f
sasidharan10 <ksasidharan98@gmail.com>
2024-02-05 13:44:23 +0530

    dc

--- cp tricks/dummy.java
+++ cp tricks/dummy.java
@@ -1,35 +1,53 @@
 import java.util.*;
 
 public class dummy {
-
-    public static List<Integer> sequentialDigits(int low, int high) {
-
-        int[] mp = { 12, 23, 34, 45, 56, 67, 78, 89, 123, 234, 345, 456,
-                567, 678, 789, 1234, 2345, 3456, 4567, 5678, 6789, 12345, 23456, 34567,
-                45678, 56789, 123456, 234567, 345678, 456789, 1234567, 2345678, 3456789,
-                12345678, 23456789, 123456789 };
-
-        int n = mp.length;
-        List<Integer> ans = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            if (mp[i] < low)
-                continue;
-
-            if (mp[i] > high)
-                break;
-
-            ans.add(mp[i]);
+    public static String minWindow(String s, String t) {
+        int n = s.length();
+        int m = t.length();
+        if (m > n)
+            return "";
+        HashMap<Character, Integer> mp = new HashMap<>();
+        for (int i = 0; i < m; i++) {
+            mp.put(t.charAt(i), mp.getOrDefault(t.charAt(i), 0) + 1);
         }
-        return ans;
+        int i = 0, j = 0;
+        int start = 0;
+        int requiredCnt = m;
+        int mini = Integer.MAX_VALUE;
+        while (j < n) {
+            char ch1 = s.charAt(j);
+            if (mp.containsKey(ch1) && mp.get(ch1) > 0)
+                requiredCnt--;
+            mp.put(ch1, mp.getOrDefault(ch1, 0) - 1);
+
+            while (requiredCnt == 0) {
+                int currentWindow = j - i + 1;
+                if (currentWindow < mini) {
+                    mini = currentWindow;
+                    start = i;
+                }
+                char ch2 = s.charAt(i);
+                mp.put(ch2, mp.getOrDefault(ch2, 0) + 1);
+                if (mp.containsKey(ch2) && mp.get(ch2) > 0) {
+                    requiredCnt++;
+                }
+                i++;
+            }
+            j++;
+        }
+        return (mini == Integer.MAX_VALUE) ? "sas" : s.substring(start, start + mini);
     }
 
     public static void main(String[] args) {
-        int[] nums = { 1, 3, 4, 8, 7, 9, 3, 5, 1 };
-        int k = 2;
+        int[] nums = { 1, 4, 1, 5, 7, 3, 6, 1, 9, 9, 3 };
+        int k = 4;
+        String str = "ADOBECODEBANC", t = "ABC";
         int low = 1000, high = 13000;
         System.out.println("Result: ");
-        // int[][] ans = divideArray(nums, k);
-        List<Integer> ans = sequentialDigits(low, high);
+        String ans = minWindow(str, t);
+        // System.out.println(minWindow(str, t));
+        // int[][] ans = maxSumAfterPartitioning(nums, k);
+        // List<Integer> ans = sequentialDigits(low, high);
         System.out.print(ans);
         // for (int[] is : ans) {
         // for (int is2 : is) {
