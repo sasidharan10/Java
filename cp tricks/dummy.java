424727d68ce133c1f06021c8994f9f9835ab8727
sasidharan10 <ksasidharan98@gmail.com>
2024-02-12 15:01:30 +0530

    dc

--- cp tricks/dummy.java
+++ cp tricks/dummy.java
@@ -1,54 +1,59 @@
 import java.util.*;
 
 public class dummy {
-    public static String minWindow(String s, String t) {
-        int n = s.length();
-        int m = t.length();
-        if (m > n)
-            return "";
-        HashMap<Character, Integer> mp = new HashMap<>();
-        for (int i = 0; i < m; i++) {
-            mp.put(t.charAt(i), mp.getOrDefault(t.charAt(i), 0) + 1);
-        }
-        int i = 0, j = 0;
-        int start = 0;
-        int requiredCnt = m;
-        int mini = Integer.MAX_VALUE;
-        while (j < n) {
-            char ch1 = s.charAt(j);
-            if (mp.containsKey(ch1) && mp.get(ch1) > 0)
-                requiredCnt--;
-            mp.put(ch1, mp.getOrDefault(ch1, 0) - 1);
-
-            while (requiredCnt == 0) {
-                int currentWindow = j - i + 1;
-                if (currentWindow < mini) {
-                    mini = currentWindow;
-                    start = i;
-                }
-                char ch2 = s.charAt(i);
-                mp.put(ch2, mp.getOrDefault(ch2, 0) + 1);
-                if (mp.containsKey(ch2) && mp.get(ch2) > 0) {
-                    requiredCnt++;
-                }
-                i++;
+    public static List<Integer> majorityElement(int[] nums) {
+        int n = nums.length;
+        int el1 = Integer.MIN_VALUE, el2 = Integer.MIN_VALUE;
+        int cnt1 = 0, cnt2 = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == el1) {
+                cnt1++;
+            } else if (cnt1 == 0 && nums[i] != el2) {
+                cnt1 = 1;
+                el1 = nums[i];
+            } else if (nums[i] == el2) {
+                cnt2++;
+            } else if (cnt2 == 0 && nums[i] != el1) {
+                cnt2 = 1;
+                el2 = nums[i];
+            } else {
+                cnt1--;
+                cnt2--;
             }
-            j++;
         }
-        return (mini == Integer.MAX_VALUE) ? "sas" : s.substring(start, start + mini);
+        cnt1 = 0;
+        cnt2 = 0;
+        List<Integer> ans = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (el1 == nums[i])
+                cnt1++;
+            if (el2 == nums[i])
+                cnt2++;
+        }
+        if (cnt1 > (n / 3))
+            ans.add(el1);
+        if (cnt2 > (n / 3))
+            ans.add(el2);
+        return ans;
     }
 
     public static void main(String[] args) {
-        int[] nums = { 1, 4, 1, 5, 7, 3, 6, 1, 9, 9, 3 };
+        int[] nums = { 1, 2 };
+        int[][] grid = { { 3, 1, 1 },
+                { 2, 5, 1 },
+                { 1, 5, 5 },
+                { 2, 1, 1 } };
         int k = 4;
-        String str = "ADOBECODEBANC", t = "ABC";
+        int n = 13;
+        // String str = "Aabb";
+        String str = "cccaaa";
         int low = 1000, high = 13000;
         System.out.println("Result: ");
-        String ans = minWindow(str, t);
-        // System.out.println(minWindow(str, t));
+        // String ans = minWindow(str, t);
+        System.out.println(majorityElement(nums));
         // int[][] ans = maxSumAfterPartitioning(nums, k);
         // List<Integer> ans = sequentialDigits(low, high);
-        System.out.print(ans);
+        // System.out.print(ans);
         // for (int[] is : ans) {
         // for (int is2 : is) {
         // System.out.print(is2 + " ");
