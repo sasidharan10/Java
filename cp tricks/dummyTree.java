5edef9f39e14976bc7452ab0297536f6f7f617d5
sasidharan10 <ksasidharan98@gmail.com>
2024-02-26 12:53:01 +0530

    dc

--- cp tricks/dummyTree.java
+++ cp tricks/dummyTree.java
@@ -5,52 +5,40 @@ public class dummyTree {
         int val;
         TreeNode left;
         TreeNode right;
-    
+
         TreeNode() {
         }
-    
+
         TreeNode(int val) {
             this.val = val;
         }
-    
+
         TreeNode(int val, TreeNode left, TreeNode right) {
             this.val = val;
             this.left = left;
             this.right = right;
         }
     }
-    public static int findPaths(TreeNode root, int val)
-    {
-        // TC: O(n)
-        // SC: O(h) stack space
 
-        if (root==null)
-            return 0;
-        if (root.left==null && root.right==null)
-        {
-            val = (val^(1<<root.val));
-            if ((val & (val - 1)) == 0)
-                return 1;
-            else
-                return 0;
-        }
-        val = (val^(1<<root.val));
-        int lt = findPaths(root.left, val);
-        int rt = findPaths(root.right, val);
-        return lt + rt;
+    public static boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null || q == null)
+            return (p == q);
+        return (p.val == q.val) && isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
     }
-    public static int pseudoPalindromicPaths (TreeNode root) {
-        return findPaths(root, 0);
-    }
+
     public static void main(String[] args) {
-    TreeNode root = null;
-    root = new TreeNode(2);
-    root.left = new TreeNode(3);
-    root.right = new TreeNode(1);
-    root.left.left = new TreeNode(3);
-    root.left.right = new TreeNode(1);
-    root.right.right = new TreeNode(1);
-    System.out.print("Result: ");
-    System.out.print(pseudoPalindromicPaths(root));
+        TreeNode root = null;
+        root = new TreeNode(2);
+        root.left = new TreeNode(3);
+        root.right = new TreeNode(1);
+        root.left.left = new TreeNode(3);
+        root.left.right = new TreeNode(1);
+        root.right.right = new TreeNode(1);
+
+        TreeNode root2 = null;
+        root2 = new TreeNode(2);
+        root2.left = new TreeNode(3);
+        System.out.print("Result: ");
+        System.out.print(isSameTree(root, root2));
     }
 }
